dbbed27191239eaac5312285d3c68869d4905f81
Aldiensyah209 <aldiensyah@webmail.umm.ac.id>
2023-09-18 23:56:43 +0700

    Navbar Landing Page

---
+++ resources/views/LandingPage/home.blade.php
@@ -0,0 +1,25 @@
+@extends('LandingPage.home')
+@section('title', 'Home')
+@section('kontent')
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-8 d-flex align-items-strech">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+@endsection
