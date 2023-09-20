0c3b4e6ba9d9b81e9556b7df1120678834185f64
Aldiensyah209 <aldiensyah@webmail.umm.ac.id>
2023-09-20 22:19:31 +0700

    Pasang Sweeper

--- resources/views/LandingPage/home.blade.php
+++ resources/views/LandingPage/home.blade.php
@@ -1,14 +1,37 @@
 @extends('LandingPage.index')
 @section('kontent')
-
-<div class="row">
-    <div class="col">
-        <h1>{{ $home->first()->judul }}</h1>
-        <h3>{{ $home->first()->deskripsi }}</h3>
+    <div class="row">
+        <div class="col d-flex flex-column justify-content-center">
+            <h1>{{ $home->first()->judul }}</h1>
+            <h3>{{ $home->first()->deskripsi }}</h3>
+        </div>
+        <div class="col">
+            <div id="carouselExampleAutoplaying" class="carousel  rounded slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    @foreach ($homeImage as $index => $item)
+                        <button type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide-to="{{ $index }}" class=" {{ $index == 0 ? 'active' : '' }}"
+                            aria-current="true" aria-label="{{ $item->gambar }}"></button>
+                    @endforeach
+                </div>
+                <div class="carousel-inner rounded">
+                    @foreach ($homeImage as $index => $item)
+                        <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                            <img src="{{ asset('images/post/landingPage/' . $item->gambar) }}" alt="Gambar">
+                        </div>
+                    @endforeach
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
     </div>
-    <div class="col">
-       
-    </div>
-</div>
-
 @endsection
