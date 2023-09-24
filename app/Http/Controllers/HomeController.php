9f5b528c9554dac2b3a36f816c49b532c71d95ef
andrechynt <m.andrechynt@gmail.com>
2023-09-24 22:02:49 +0700

    add new produk

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\About;
+use App\Models\BintangKonveksi;
 use App\Models\Home;
 use App\Models\HomeImage;
 use App\Models\Smartbuy;
+use App\Models\Xuzu;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -15,6 +17,8 @@ class HomeController extends Controller
         $homeImage = HomeImage::all();
         $about = About::all();
         $smartBuy = Smartbuy::all();
-        return view('landingPage.home', compact('home', 'homeImage', 'about', 'smartBuy'));
+        $xuzu = Xuzu::latest()->take(2)->get();
+        $bintangKonveksi = BintangKonveksi::latest()->take(2)->get();
+        return view('landingPage.home', compact('home', 'homeImage', 'about', 'smartBuy', 'xuzu', 'bintangKonveksi'));
     }
 }
